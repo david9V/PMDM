73af0a60c9778e0e2122e48ee4331e6d9c20aa1b
david9V <davidvilches818@gmail.com>
2022-10-26 14:40:24 +0200

    26/10

--- backupPenDrive/Prueba2/app/src/main/java/com/example/prueba2/wordle/MainActivity.java
+++ backupPenDrive/Prueba2/app/src/main/java/com/example/prueba2/wordle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.prueba2.wordle;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -49,13 +50,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button bX;
     Button bY;
     Button bZ;
+    Button bEnviar;
+    Button bBorrar;
+
+    TextView cuadroLetra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wordle);
-
-
 
         bA = findViewById(R.id.bA);
         bB = findViewById(R.id.bB);
@@ -84,6 +87,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bX = findViewById(R.id.bX);
         bY = findViewById(R.id.bY);
         bZ = findViewById(R.id.bZ);
+        bEnviar = findViewById(R.id.bZ);
+        bBorrar = findViewById(R.id.bZ);
 
         bA.setOnClickListener(this);
         bB.setOnClickListener(this);
@@ -112,6 +117,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bX.setOnClickListener(this);
         bY.setOnClickListener(this);
         bZ.setOnClickListener(this);
+        bEnviar.setOnClickListener(this);
+        bBorrar.setOnClickListener(this);
+
+        rellenarIds();
     }
 
 
@@ -120,83 +129,105 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         switch (id) {
             case R.id.bA:
+                añadirLetra(bA);
                 break;
             case R.id.bB:
+                añadirLetra(bB);
                 break;
             case R.id.bCC:
+                añadirLetra(bC);
                 break;
             case R.id.bD:
+                añadirLetra(bD);
                 break;
             case R.id.bE:
+                añadirLetra(bE);
                 break;
             case R.id.bF:
+                añadirLetra(bF);
                 break;
             case R.id.bG:
+                añadirLetra(bG);
                 break;
             case R.id.bH:
+                añadirLetra(bH);
                 break;
             case R.id.bI:
+                añadirLetra(bI);
                 break;
             case R.id.bJ:
+                añadirLetra(bJ);
                 break;
             case R.id.bK:
+                añadirLetra(bK);
                 break;
             case R.id.bL:
+                añadirLetra(bL);
                 break;
             case R.id.bM:
+                añadirLetra(bM);
                 break;
             case R.id.bN:
+                añadirLetra(bN);
                 break;
             case R.id.bÑ:
+                añadirLetra(bÑ);
                 break;
             case R.id.bO:
+                añadirLetra(bO);
                 break;
             case R.id.bP:
+                añadirLetra(bP);
                 break;
             case R.id.bQ:
+                añadirLetra(bQ);
                 break;
             case R.id.bR:
+                añadirLetra(bR);
                 break;
             case R.id.bS:
+                añadirLetra(bS);
                 break;
             case R.id.bT:
+                añadirLetra(bT);
                 break;
             case R.id.bU:
+                añadirLetra(bU);
                 break;
             case R.id.bV:
+                añadirLetra(bV);
                 break;
             case R.id.bW:
+                añadirLetra(bW);
                 break;
             case R.id.bX:
+                añadirLetra(bX);
                 break;
             case R.id.bY:
+                añadirLetra(bY);
                 break;
             case R.id.bZ:
+                añadirLetra(bZ);
+                break;
+            case R.id.bEnviar:
+                añadirLetra(bZ);
+                break;
+            case R.id.bBorrar:
+
                 break;
         }
     }
 
-    public void añadirLetra(Button b, int fila, int columna){
-        switch (columna){
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            case 6:
-
-                break;
+    public void añadirLetra(Button b){
+        cuadroLetra = findViewById(idRecuadros[filaActual][columnaActual]);
+        cuadroLetra.setText(b.getText().toString());
+        if (columnaActual < 5){
+            columnaActual++;
         }
+    }
+
+    public void borrar(){
+
     }
 
     public void rellenarIds(){
