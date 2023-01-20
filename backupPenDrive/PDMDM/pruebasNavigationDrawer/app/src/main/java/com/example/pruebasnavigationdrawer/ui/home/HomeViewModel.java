91038f04dd59c249b857f43cdc8a96ef1d716c16
david9V <davidvilches818@gmail.com>
2023-01-20 10:34:13 +0100

    20/01

--- backupPenDrive/PDMDM/pruebasNavigationDrawer/app/src/main/java/com/example/pruebasnavigationdrawer/ui/home/HomeViewModel.java
+++ backupPenDrive/PDMDM/pruebasNavigationDrawer/app/src/main/java/com/example/pruebasnavigationdrawer/ui/home/HomeViewModel.java
@@ -1,5 +1,7 @@
 package com.example.pruebasnavigationdrawer.ui.home;
 
+import android.widget.ImageView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -10,7 +12,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
     }
 
     public LiveData<String> getText() {
