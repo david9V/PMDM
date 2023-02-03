770c194c7f11b34846ca6ece08fc2d068bee92e0
david9V <davidvilches818@gmail.com>
2023-02-03 14:36:44 +0100

    02/23

--- backupPenDrive/EclipseWorkSpace/Ejercicio_Eventos/src/main/java/david/david/Main.java
+++ backupPenDrive/EclipseWorkSpace/Ejercicio_Eventos/src/main/java/david/david/Main.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.bson.Document;
+
 public class Main {
 
-	static MongoDAO dao = new MongoDAO("eventos", "eventos");
+	static MongoDAO dao = new MongoDAO("eventos", "prueba");
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,24 +20,23 @@ public class Main {
 			opc = Integer.valueOf(br.readLine());
 			switch (opc) {
 			case 1:{
-				System.out.println("sdfsdf");
-				//insertarDocumento();
+				insertarDocumento();
 				break;
 			}
 			case 2:{
-				
+				eliminarDocumento();
 				break;
 			}
 			case 3:{
-				
+				eliminarCollecion();
 				break;
 			}
 			case 4:{
-				
+				eliminarTodosLosDocumentos();
 				break;
 			}
 			case 5:{
-				
+				consultarDocumento();
 				break;
 			}
 			case 6:{
@@ -43,11 +44,11 @@ public class Main {
 				break;
 			}
 			case 7:{
-				
+				consultarColleccionAscendente();
 				break;
 			}
 			case 8:{
-				
+				consultarColleccionDescendente();
 				break;
 			}
 			case 9:{
@@ -55,7 +56,7 @@ public class Main {
 				break;
 			}
 			}
-		} while(opc != 5);
+		} while(opc < 10);
 		
 		/*
 		List<Document> consulta = collecion.find().into(new ArrayList<Document>());
@@ -96,15 +97,72 @@ public class Main {
 	static void insertarDocumento() throws IOException {
 		System.out.println("Introduzca la id");
 		String id = br.readLine();
-		System.out.println("Introduzca el nombre");
-		String nombre = br.readLine();
-		System.out.println("Introduzca la fecha");
-		String fecha = br.readLine();
-		System.out.println("Introduzca la ciudad");
-		String ciudad = br.readLine();
-		System.out.println("Introduzca el precio");
-		int precio = Integer.valueOf(br.readLine());
-		dao.insertarDocumento(null);
+		if (dao.consultarDocumento(id) == null) {
+			System.out.println("Introduzca el nombre");
+			String nombre = br.readLine();
+			System.out.println("Introduzca la fecha");
+			String fecha = br.readLine();
+			System.out.println("Introduzca la ciudad");
+			String ciudad = br.readLine();
+			System.out.println("Introduzca el precio");
+			int precio = Integer.valueOf(br.readLine());
+			dao.insertarDocumento(new Document()
+					.append("_id", id)
+					.append("nombre", nombre)
+					.append("fecha", fecha)
+					.append("ciudad", ciudad)
+					.append("precio", precio));
+		}
+		else
+			System.out.println("Ya existe un documento con esa id");
+		
+	}
+	
+	static void eliminarDocumento() throws IOException {
+		System.out.println("Introduzca la id del documento");
+		String id = br.readLine();
+		if (dao.consultarDocumento(id) != null) {
+			System.out.println("¿Está seguro de que desea borrar el siguiente documento? (S/N)");
+			System.out.println(dao.consultarDocumento(id).toJson());
+			String o = br.readLine();
+			if (o.equals("S")) {
+				dao.eliminarDocumento(id);
+				System.out.println("Documento borrado");
+			}
+			else {
+				System.out.println("Operación de borrado cancelada");
+			}
+		}
+		else {
+			System.out.println("Error de borrado. No se ha encontrado un documento con esa id");
+		}
+		
+	}
+	
+	static void eliminarCollecion() {
+		dao.eliminarCollecion();
+	}
+	
+	static void eliminarTodosLosDocumentos() {
+		dao.eliminarTodosDocumentosDeCollecion();
+	}
+	
+	static void consultarDocumento() throws IOException {
+		System.out.println("Introduzca la id");
+		String id = br.readLine();
+		try {
+			System.out.println(dao.consultarDocumento(id).toJson());			
+		} catch(NullPointerException e) {
+			System.out.println("Documento no encontrado");
+		}
+	}
+	
+	static void consultarColleccionAscendente() {
+		System.out.println(dao.consultarCollecionOrdenadaAscendente());
+	}
+	
+	static void consultarColleccionDescendente() {
+		System.out.println(dao.consultarCollecionOrdenadaDescendente());
 	}
 	
 	static void opciones() {
@@ -112,7 +170,7 @@ public class Main {
 		System.out.println("1- Insertar documento");
 		System.out.println("2- Eliminar documento de la colección");
 		System.out.println("3- Eliminar colección");
-		System.out.println("4- Borrar todos los documentos de la colección");
+		System.out.println("4- Eliminar todos los documentos de la colección");
 		System.out.println("5- Consultar un documento");
 		System.out.println("6- Consultar usando agregaciones");
 		System.out.println("7- Consultar colección de forma ordenada ascendente");
--- backupPenDrive/EclipseWorkSpace/Ejercicio_Eventos/src/main/java/david/david/MongoDAO.java
+++ backupPenDrive/EclipseWorkSpace/Ejercicio_Eventos/src/main/java/david/david/MongoDAO.java
@@ -2,6 +2,9 @@ package david.david;
 
 import org.bson.Document;
 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.result.DeleteResult;
+
 public class MongoDAO {
 	
 	MongoConnection mongoConnection;
@@ -14,32 +17,37 @@ public class MongoDAO {
 		mongoConnection.getColleccion().insertOne(document);
 	}
 	
-	public void eliminarDocumento() {
-		
+	public boolean eliminarDocumento(String id) {
+		DeleteResult deleteResult = mongoConnection.getColleccion().deleteOne(new Document("_id", id));
+		if (deleteResult.getDeletedCount() == 1)
+			return true;
+		else
+			return false;
 	}
 	
 	public void eliminarCollecion() {
-		
+		mongoConnection.getColleccion().drop();
 	}
 	
-	public void borrarTodosDocumentosDeCollecion() {
-		
+	public void eliminarTodosDocumentosDeCollecion() {
+		mongoConnection.getColleccion().deleteMany(new Document());
 	}
 	
-	public void consultarDocumento() {
-		
+	public Document consultarDocumento(String id) {
+		FindIterable<Document> it = mongoConnection.getColleccion().find(new Document("_id", id));
+		return it.first();
 	}
 	
 	public void consultarCollecionConAgregacion() {
 		
 	}
 	
-	public void consultarCollecionOrdenadaAscendente() {
-		
+	public FindIterable<Document> consultarCollecionOrdenadaAscendente() {
+		return mongoConnection.getColleccion().find().sort(new Document("_id", 1));
 	}
 	
-	public void consultarCollecionOrdenadaDescendente() {
-		
+	public FindIterable<Document> consultarCollecionOrdenadaDescendente() {
+		return mongoConnection.getColleccion().find().sort(new Document("_id", 0));
 	}
 	
 	public void actualizarDocumento() {
